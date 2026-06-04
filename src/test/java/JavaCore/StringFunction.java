70b1b48a23a5647eb92df1ef31b3edfad8c30457
vermaankit29899-ai <verma.ankit29899@gmail.com>
2026-06-04 16:11:19 +0530

    Update StringFunction.java

--- src/test/java/JavaCore/StringFunction.java
+++ src/test/java/JavaCore/StringFunction.java
@@ -5,8 +5,8 @@ public class StringFunction {
     public static void main(String[] args)
     {
 
-        String s1 = "Ankit";
-        String s2 = new String("Ankit");
+        String s1 = "Ankit"; //liternal
+        String s2 = new String("Ankit"); //Using new Keyword
 
         System.out.println(s1==s2);  //false
         System.out.println(s1.equals(s2)); //true
